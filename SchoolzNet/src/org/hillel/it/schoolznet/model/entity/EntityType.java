cda6813ac1391c65b3af2acaa22e99a1b78106db
ithillelVladimirRyabinskiy <Cuifey@gmail.com>
2013-04-17 00:43:00 +0300

    fixes

---
+++ SchoolzNet/src/org/hillel/it/schoolznet/model/entity/EntityType.java
@@ -0,0 +1,6 @@
+package org.hillel.it.schoolznet.model.entity;
+
+public enum EntityType {
+	ALBUM, COMMENT, FRIENDREQUEST, GROUP, MESSAGE, PICTURE, USER, VIDEO
+	//dolwen li butb tyt konstpyktop???
+}
